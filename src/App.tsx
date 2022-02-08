82070aa627866875c96484ec2c778d7adf0ceef8
manselli <massimiliano.manselli@gmail.com>
2022-02-08 11:26:30 +0100

    7 commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
    <>
   <Navigation />
   <Routes>
-    <Route path="/cali_scelfo" element={<Home />} />
+    <Route path="/cali_scelfo/" element={<Home />} />
     <Route path="/cali_scelfo/about" element={<About />} />
     <Route path="/cali_scelfo/contact" element={<Contact />} />
   </Routes>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -12,8 +12,9 @@ export default function Navigation() {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="/cali_scelfo/about">About</Nav.Link>
-      <Nav.Link href="/cali_scelfo/contact">Contact</Nav.Link>
+      <Nav.Link href="#features"><NavLink className="nav-link" to="/cali_scelfo/">Home</NavLink></Nav.Link>
+      <Nav.Link href="#features"><NavLink className="nav-link" to="/cali_scelfo/about">About</NavLink></Nav.Link>
+      <Nav.Link href="#features"><NavLink className="nav-link" to="/cali_scelfo/contact">Contact</NavLink></Nav.Link>
     </Nav>
   </Navbar.Collapse>
   </Container>
